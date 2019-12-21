59c83527aaa5497f812efb036eaf9b2b187731f0
cwork <colbycw@zoho.com>
2019-12-21 11:46:53 -0800

    Add copy functionality

--- src/components/Messenger/ShareMessage.js
+++ src/components/Messenger/ShareMessage.js
@@ -3,10 +3,23 @@ import MessageContext from '../../context/Messenger/messengerContext';
 
 const ShareMessage = () => {
   const { message } = useContext(MessageContext);
+  const clickHandler = () => {
+    const messageText = document.getElementById('message');
+    messageText.select();
+    document.execCommand('copy');
+  };
+
   return (
     <div>
-      <input type='text' value={`${window.location}#${message}`} readOnly />
-      <button className='btn'>Copy</button>
+      <input
+        id='message'
+        type='text'
+        value={`${window.location}#${message}`}
+        readOnly
+      />
+      <button onClick={clickHandler} className='btn'>
+        Copy
+      </button>
     </div>
   );
 };
