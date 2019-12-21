368c6f94b6d4ae196d6889c385b6d577abe9b4fc
cwork <colbycw@zoho.com>
2019-12-21 10:01:52 -0800

    Initialize MessageState

---
+++ src/context/Messenger/MessengerState.js
@@ -0,0 +1,18 @@
+import React, { useReducer } from 'react';
+import MessengerContext from './messengerContext';
+import MessengerReducer from './messengerReducer';
+
+const MessengerState = props => {
+  const initialState = {
+    message: '',
+    hasMessage: false
+  };
+
+  const [state, dispatch] = useReducer(MessengerReducer, initialState);
+
+  return (
+    <MessengerContext.Provider value={{}}>
+      {props.children}
+    </MessengerContext.Provider>
+  );
+};
