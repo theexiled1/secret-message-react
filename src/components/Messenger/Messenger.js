0bfbdbea9ed8919ee05d8743b5ebec5f40751374
cwork <colbycw@zoho.com>
2019-12-21 10:48:35 -0800

    Render Card on Messenger component

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react';
 import CreateMessageForm from './CreateMessageForm';
 import ShareMessage from './ShareMessage';
+import Card from '../Layout/Card';
 import MessageContext from '../../context/Messenger/messengerContext';
 
 const Messenger = () => {
   const { message } = useContext(MessageContext);
-  return <div>{message === '' ? <CreateMessageForm /> : <ShareMessage />}</div>;
+  return (
+    <div className='row'>
+      <Card>{message === '' ? <CreateMessageForm /> : <ShareMessage />}</Card>
+    </div>
+  );
 };
 
 export default Messenger;
