6c2ba7187424423c48a28c66886cd3f0c5b48752
cwork <colbycw@zoho.com>
2019-12-21 10:20:20 -0800

    Add and render ShareMessage component

--- src/components/Messenger/Messenger.js
+++ src/components/Messenger/Messenger.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CreateMessageForm from './CreateMessageForm';
+import ShareMessage from './ShareMessage';
 
 const Messenger = () => {
   return (
     <div>
       <CreateMessageForm />
+      <ShareMessage />
     </div>
   );
 };
---
+++ src/components/Messenger/ShareMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShareMessage = () => {
+  return (
+    <div>
+      <input type='text' readOnly />
+      <button className='btn'>Copy</button>
+    </div>
+  );
+};
+
+export default ShareMessage;
