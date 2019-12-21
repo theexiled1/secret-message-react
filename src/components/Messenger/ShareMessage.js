427b4166b1ad91064c6f5a1936903ab300cfc5ab
cwork <colbycw@zoho.com>
2019-12-21 11:38:57 -0800

    Add hash to url

--- src/components/Messenger/ShareMessage.js
+++ src/components/Messenger/ShareMessage.js
@@ -5,7 +5,7 @@ const ShareMessage = () => {
   const { message } = useContext(MessageContext);
   return (
     <div>
-      <input type='text' value={`${window.location}${message}`} readOnly />
+      <input type='text' value={`${window.location}#${message}`} readOnly />
       <button className='btn'>Copy</button>
     </div>
   );
